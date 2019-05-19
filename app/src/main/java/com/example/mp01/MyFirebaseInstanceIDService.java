a02fd3410b4bdb1d58778b1d1ccc319d1ed48dc0
jaeeesin <adamsin@naver.com>
2019-05-19 13:29:47 +0900

    push notification first version

---
+++ app/src/main/java/com/example/mp01/MyFirebaseInstanceIDService.java
@@ -0,0 +1,28 @@
+package com.example.mp01;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebase";
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token){
+        //기타 작업으로 활용
+
+    }
+
+}
+
